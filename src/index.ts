063989e064b859efd66429dad6f84d28bae619bd
rzoff <rodrigo@zoff.tech>
2020-07-26 21:11:18 -0300

    fix crete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 'use strict';
 import { FirebaseSwaggerUI, IAppOptions } from "./middleware/firebase-swagger-ui";
 
-export function create(definitionPath: string, appOptions:IAppOptions): FirebaseSwaggerUI {
-  return new FirebaseSwaggerUI(definitionPath, appOptions);
+export function create(appOptions: IAppOptions): FirebaseSwaggerUI {
+  return new FirebaseSwaggerUI(appOptions);
 }
--- src/middleware/firebase-swagger-ui.ts
+++ src/middleware/firebase-swagger-ui.ts
@@ -4,53 +4,32 @@ import * as express from 'express';
 import cookieParser = require('cookie-parser');
 import { SwaggerUI } from './swagger.ui';
 
-import * as logger from 'morgan';
 import * as fs from 'fs';
 import * as jsyaml from 'js-yaml';
 
-import * as url from 'url';
-
 export interface IAppOptions {
-    prefix: string;
-    logging?: string;
-    routing: any;
+    oas?: any;
+    oasPathFile?: string;
     publicFolderPath: string;
 }
 
 export class FirebaseSwaggerUI {
     private app: express.Application;
     private swaggerDocUI: any;
-    public prefix: string = "core";
 
-    constructor(definitionPath: string, appOptions: IAppOptions) {
+    constructor(appOptions: IAppOptions) {
         try {
-            appOptions.prefix = appOptions.prefix.toLowerCase()
-            this.prefix = appOptions.prefix;
-
-            console.log("Se crea la app swagger ui " + appOptions.prefix);
             this.app = express();
-
-            console.log("Se inicia la obtencion del yaml desde el archivo");
-
-            const spec = fs.readFileSync(definitionPath, 'utf8');
-            const swaggerDoc = jsyaml.safeLoad(spec);
-
-            console.log("Se obtuvo el yaml desde el archivo");
-
-            for (var pathService in swaggerDoc.paths) {
-                var service = swaggerDoc.paths[pathService];
-                for (const methodName in service) {
-                    delete swaggerDoc.paths[pathService];
-                    const method = service[methodName];
-           
-                    let controllerName = method["x-swagger-router-controller"];
-                    method["x-swagger-router-controller"] = controllerName.charAt(0).toUpperCase() + controllerName.slice(1);
-                    swaggerDoc.paths["/" + appOptions.prefix.toLowerCase() + controllerName.toLowerCase() + pathService.toLowerCase()] = service;
-                    break;
+            let swaggerDoc = appOptions.oas;
+            if (!swaggerDoc) {
+                if (appOptions.oasPathFile) {
+                    const fileOAS = fs.readFileSync(appOptions.oasPathFile, 'utf8');
+                    swaggerDoc = jsyaml.safeLoad(fileOAS);
+                } else {
+                    throw new Error("oas or oasPathFile are required");
                 }
             }
 
-            //----------------------------------------------------
             this.app.use(express.text());
             this.app.use(express.json());
             this.app.use(express.urlencoded({ extended: false }));
@@ -63,13 +42,13 @@ export class FirebaseSwaggerUI {
             this.app.use(express.static(appOptions.publicFolderPath));
 
         } catch (error) {
-            console.log("swagger-ui-oas3 / Error in ExpressAppConfig Constructor / Check OAS3 Definition")
+            console.log("firebase-swagger-ui / Error in constructor / Check OAS3 Definition")
             console.error(error);
             throw error;
         }
     }
 
-    public getExpressApp(): express.Application {
+    public getAppExpress(): express.Application {
         return this.app;
     }
 
